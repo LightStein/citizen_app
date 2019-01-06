cbd160e11b0ac3e637223ce10c5987415fb9ccdc
LightStein <LightStein@users.noreply.github.com>
2019-01-06 15:03:09 +0400

    გასწორდა პროფილის სურათის ჩამოტვირთვის ბაგი

--- app/src/main/java/com/abc/citizen/Description.kt
+++ app/src/main/java/com/abc/citizen/Description.kt
@@ -283,7 +283,7 @@ class Description : AppCompatActivity(), OnMapReadyCallback {
         when (requestCode) {
             CAMERA_REQUEST_CODE -> {
                 if (resultCode == Activity.RESULT_OK && data != null) {
-                    photoImageView.setImageURI(Uri.parse(photoPath))
+                    photoImageView.setImageURI(selectedPhotoUri)
                 }
             }
             8 -> {
